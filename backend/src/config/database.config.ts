16fe2ff499c8f11c2e5189d6e3483085e8cc1693
vishv0407 <vishvboda0407@gmail.com>
2025-01-03 23:15:37 +0530

    final modification

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -7,9 +7,9 @@ export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DATABASE_HOST || 'localhost',
   port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-  username: process.env.DATABASE_USERNAME || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'vishv',
-  database: process.env.DATABASE_NAME || 'recipe_explorer',
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   autoLoadEntities: true,
   synchronize: true, // Set to false in production
 };
